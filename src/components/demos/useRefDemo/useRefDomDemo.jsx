82d6ab9a5932045505da9d62a3cbff12e5705335
Manikant Jha <manikantcodes@gmail.com>
2023-10-16 16:47:49 +0530

    useRef hooks example updated

--- src/components/demos/useRefDemo/useRefDomDemo.jsx
+++ src/components/demos/useRefDemo/useRefDomDemo.jsx
@@ -1,15 +1,19 @@
 import React, { useRef } from "react";
 
 function UseRefDomDemo() {
-  const inputElement = useRef();
+  const inputElement = useRef(null);
+
+  console.log("inputElement", inputElement);
 
   const focusInput = () => {
+    console.log("inputElement", inputElement);
     inputElement.current.focus();
+    inputElement.current.size = 500;
   };
 
   return (
     <>
-      <input type="text" ref={inputElement} />
+      <input type="text" id="input" ref={inputElement} />
       <button onClick={focusInput}>Focus Input</button>
     </>
   );
--- src/components/demos/useRefDemo/useRefPersistanceDemo.jsx
+++ src/components/demos/useRefDemo/useRefPersistanceDemo.jsx
@@ -2,12 +2,15 @@ import React, { useEffect, useRef, useState } from "react";
 
 function UseRefPersistanceDemo() {
   const [inputValue, setInputValue] = useState("");
+  const prevState = useRef(null);
   const count = useRef(0);
-  //   const [renderCount, setRenderCount] = useState(0);
+
+  // const [renderCount, setRenderCount] = useState(0);
 
   useEffect(() => {
     count.current++;
-    // setRenderCount((prevRenderCount) =>  prevRenderCount + 1);
+    prevState.current = inputValue;
+    // setRenderCount((prevRenderCount) => prevRenderCount + 1);
   }, [inputValue]);
 
   console.log("Rendered");
@@ -18,8 +21,19 @@ function UseRefPersistanceDemo() {
         type="text"
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
+        id="input"
       />
+
+      <button
+        onClick={() => {
+          count.current++;
+          console.log(count);
+        }}
+      >
+        ++
+      </button>
       <h1>Render Count: {count.current}</h1>
+      <h1>{prevState.current}</h1>
       {/* <h1>Render Count: {renderCount}</h1> */}
     </>
   );
